fcb6b6baa2094e970a0fe2ae276d67c7236218c4
niravs200 <niravs200@gmail.com>
2019-02-22 23:56:43 -0400

    Making changes to BookDAO and UserDAO interface

--- src/main/java/com/library/DAO/IBookDAO.java
+++ src/main/java/com/library/DAO/IBookDAO.java
@@ -6,11 +6,12 @@ import com.library.POJO.Book;
 
 public interface IBookDAO {
 	public Book getBookByID(int itemID);
+	public Book getBookByISBN(int bookISBN);
 	public ArrayList<Book> getBookByTitle(String bookTitle);
 	public ArrayList<Book> getBooksByAuthor(String bookAuthor);
 	public ArrayList<Book> getBookByPublisher(String bookPublisher);
 	public ArrayList<Book> getBookByDescription(String bookDescription);
-	public Book getBookByISBN(int bookISBN);
+	public String getBookTitle(int itemID);
 	public void deleteBookByID(int itemID);
 	public void createBook(Book book);
 	public void updateBook(Book book);
