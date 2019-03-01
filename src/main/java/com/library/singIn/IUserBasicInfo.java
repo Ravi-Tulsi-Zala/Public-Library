69b3db96f71f97e193fa9e8f70676eb3bdf5a30b
devanshuShrivastava <devanshu.srivastava1@gmail.com>
2019-03-01 03:12:08 -0400

    Intermediate commit to signIn feature.

---
+++ src/main/java/com/library/singIn/IUserBasicInfo.java
@@ -0,0 +1,8 @@
+package com.library.singIn;
+
+public interface IUserBasicInfo {
+	public String getEmail();
+	public String getPwd();
+	public void setEmail(String m);
+	public void  setPwd(String n);
+}
--- src/main/java/com/library/singIn/SignInController.java
+++ src/main/java/com/library/singIn/SignInController.java
@@ -1,5 +1,16 @@
 package com.library.singIn;
 
+
 public class SignInController {
 
+	IUserBasicInfo basic;
+	User user;
+//	SignUpMocked mockDataBase;
+
+	
+	public SignInController(IUserBasicInfo basic) {
+		this.basic = basic;
+//		clientValidation();
+	}
+	
 }
