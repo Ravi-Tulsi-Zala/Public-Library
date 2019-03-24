ac4f4e628c38a131c409dd911a5e15248918c7c0
Eugene Shishlannikov <eg343462@dal.ca>
2019-03-24 19:41:58 -0300

    testing code line counter

---
+++ src/main/java/com/library/signOut/SignOutController.java
@@ -0,0 +1,40 @@
+package com.library.signOut;
+
+public class SignOutController {
+	private String string1 = null;
+	private String string2 = null;
+	private String string3 = null;
+	private String string4 = null;
+	private String string5 = null;
+	public String getString1() {
+		return string1;
+	}
+	public void setString1(String string1) {
+		this.string1 = string1;
+	}
+	public String getString2() {
+		return string2;
+	}
+	public void setString2(String string2) {
+		this.string2 = string2;
+	}
+	public String getString3() {
+		return string3;
+	}
+	public void setString3(String string3) {
+		this.string3 = string3;
+	}
+	public String getString4() {
+		return string4;
+	}
+	public void setString4(String string4) {
+		this.string4 = string4;
+	}
+	public String getString5() {
+		return string5;
+	}
+	public void setString5(String string5) {
+		this.string5 = string5;
+	}
+
+}
