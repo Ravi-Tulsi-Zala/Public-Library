7f66ac403d082310dc924a90e67a0ef28764e47c
Eugene Shishlannikov <eg343462@dal.ca>
2019-04-03 21:21:35 -0300

    Adding test for DBSearchController

--- src/test/java/com/library/search/MockSearchRequest.java
+++ src/test/java/com/library/search/MockSearchRequest.java
@@ -1,38 +1,42 @@
 package com.library.search;
 
-public class SearchRequestMock implements ISearchRequest {
+import java.util.LinkedList;
+import java.util.List;
+
+import com.library.businessModels.Book;
+import com.library.businessModels.LibraryItem;
+import com.library.businessModels.Movie;
+import com.library.businessModels.Music;
+
+public class MockSearchRequest extends SearchRequest {
 	private ISearchResults searchResults = SearchFactory.instance().makeSearchResults();
 	
-	
-
-	@Override
-	public void addCategoryToSearchIn(SearchCategory categoryToSearch) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public SearchTermsAndPage getTermsAndPage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setTermsAndPage(SearchTermsAndPage termsAndPage) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean isNewSearchTerms(ISearchRequest other) {
-		// TODO Auto-generated method stub
-		return false;
+	public MockSearchRequest() {
+		List<LibraryItem> books = new LinkedList<LibraryItem>();
+		for(int i = 0; i < 15; ++i) {
+			LibraryItem book = new Book();
+			book.setAvailability(i);
+			books.add(book);
+		}
+		searchResults.addSearchResultsForCategory(books);
+		List<LibraryItem> movies = new LinkedList<LibraryItem>();
+		for(int i = 0; i < 17; ++i) {
+			LibraryItem movie = new Movie();
+			movie.setAvailability(i);
+			movies.add(movie);
+		}
+		List<LibraryItem> musics = new LinkedList<LibraryItem>();
+		searchResults.addSearchResultsForCategory(movies);
+		for(int i = 0; i < 19; ++i) {
+			LibraryItem music = new Music();
+			music.setAvailability(i);
+			musics.add(music);
+		}
+		searchResults.addSearchResultsForCategory(musics);
 	}
 
 	@Override
 	public ISearchResults searchInDb() {
-		// TODO Auto-generated method stub
-		return null;
+		return searchResults;
 	}
-
 }
