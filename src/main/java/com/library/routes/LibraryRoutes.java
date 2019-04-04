bfb7175ad4aeab0801f3dbbc81c722cfce51b12a
Ravi-Tulsi-Zala <rv901764@dal.ca>
2019-04-04 20:38:41 -0300

    Signin code comment

--- src/main/java/com/library/routes/LibraryRoutes.java
+++ src/main/java/com/library/routes/LibraryRoutes.java
@@ -1,9 +1,6 @@
 package com.library.routes;
 
-import java.io.Console;
-
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +19,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -37,28 +33,26 @@ import com.library.businessModels.Book;
 import com.library.businessModels.Cover;
 import com.library.businessModels.Display;
 import com.library.businessModels.DisplayDetailed;
-import com.library.businessModels.Book;
-import com.library.businessModels.Cover;
 import com.library.businessModels.LibraryItem;
 import com.library.businessModels.Movie;
 import com.library.businessModels.Music;
 import com.library.businessModels.User;
+import com.library.businessModels.UserItem;
 import com.library.dbConnection.DatabaseConnection;
 import com.library.forgotPassword.ForgotPasswordController;
 import com.library.forgotPassword.IForgotPasswordController;
 import com.library.forgotPassword.RecoverPassword;
 import com.library.itemDetailed.DetailedDisplayFetcher;
 import com.library.itemDetailed.IDetailedDisplayFetcher;
-import com.library.businessModels.UserItem;
 import com.library.loanmanagement.ILoanManagementController;
 import com.library.loanmanagement.Select;
 import com.library.messages.Messages;
 import com.library.parsers.JsonStringParser;
 import com.library.search.BookSearch;
 import com.library.search.IDBSearchController;
-import com.library.search.SearchFactory;
 import com.library.search.MovieSearch;
 import com.library.search.MusicSearch;
+import com.library.search.SearchFactory;
 import com.library.search.SearchRequest;
 import com.library.search.SearchResults;
 import com.library.search.SearchTermsAndPage;
@@ -67,10 +61,9 @@ import com.library.signIn.ISignInController;
 import com.library.signIn.SignInController;
 import com.library.signUp.ISignUpController;
 import com.library.signUp.SignUpController;
-import com.library.welcomePage.UserSessionDetail;
 import com.library.welcomePage.IWelcomeController;
+import com.library.welcomePage.UserSessionDetail;
 import com.library.welcomePage.WelcomePageController;
-import com.mysql.jdbc.PreparedStatement;
 
 @Controller
 public class LibraryRoutes implements WebMvcConfigurer {
@@ -207,10 +200,10 @@ public class LibraryRoutes implements WebMvcConfigurer {
 			
 			java.util.Enumeration<String> reqEnum = request.getParameterNames();
 
-			while (reqEnum.hasMoreElements()) {
-				String s = reqEnum.nextElement();
-				model.addAttribute("Error",request.getParameter(s));
-			}
+//			while (reqEnum.hasMoreElements()) {
+//				String s = reqEnum.nextElement();
+//				model.addAttribute("Error",request.getParameter(s));
+//			}
 
 			
 			ISignInController signIn = factory.signIn(user, httpSession);
