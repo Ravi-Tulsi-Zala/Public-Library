e739f1344687c2c785a5e5e6e6247e9b6d2f9423
Eugene Shishlannikov <eg343462@dal.ca>
2019-04-04 23:54:43 -0300

    emailing attempt8

--- src/main/java/com/library/email/SendEmail.java
+++ src/main/java/com/library/email/SendEmail.java
@@ -28,7 +28,7 @@ public class SendEmail {
 		props.put("mail.smtp.fallback", "true");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.starttls.required", "true");
-		props.put("mail.smtp.ssl.enable", "false");
+		props.put("mail.smtp.ssl.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
 	    props.put("mail.smtp.socketFactory.port", "587");   
