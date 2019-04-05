9186426f0b150a5a8b0459dc16d88119de2f3ece
Eugene Shishlannikov <eg343462@dal.ca>
2019-04-05 19:53:17 -0300

    adding isNewSearchTermsTest() for Searchrequest class

--- src/main/java/com/library/search/SearchFactory.java
+++ src/main/java/com/library/search/SearchFactory.java
@@ -29,4 +29,8 @@ public class SearchFactory {
 	public SearchResults makeSearchResults() {
 		return new SearchResults();
 	}
+	
+	public SearchRequest makeSearchRequest() {
+		return new SearchRequest();
+	}
 }
