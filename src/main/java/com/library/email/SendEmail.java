aeb5cfb0d805d28cc7b972ae866e295e5e09a4cf
Eugene Shishlannikov <eg343462@dal.ca>
2019-04-05 00:21:36 -0300

    emailing attempt9

--- src/main/java/com/library/email/SendEmail.java
+++ src/main/java/com/library/email/SendEmail.java
@@ -31,8 +31,8 @@ public class SendEmail {
 		props.put("mail.smtp.ssl.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
-	    props.put("mail.smtp.socketFactory.port", "587");   
-	    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");   
+//	    props.put("mail.smtp.socketFactory.port", "587");   
+//	    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");   
 		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
 		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
