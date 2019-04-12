1d10f384eeec2d950e2deccf140c0bb905fcdf5b
Ravi-Tulsi-Zala <rv901764@dal.ca>
2019-04-12 06:36:44 -0300

    Testing error page

--- src/main/java/com/library/demo/LibraryApplication.java
+++ src/main/java/com/library/demo/LibraryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.library.error.CustomErrorController;
 import com.library.localStorage.CoverImageLoaderBean;
 import com.library.routes.LibraryRoutes;
 import com.library.search.DBSeachControllerBean;
@@ -16,6 +17,7 @@ import com.library.search.SearchResultCoverImgProxyBean;
 @ComponentScan(basePackageClasses = {LibraryRoutes.class, 
 										DynamicResourcesConfiguration.class, 
 										SearchResultCoverImgProxyBean.class,
+										CustomErrorController.class,
 										DBSeachControllerBean.class,
 										CoverImageLoaderBean.class})
 public class LibraryApplication {
